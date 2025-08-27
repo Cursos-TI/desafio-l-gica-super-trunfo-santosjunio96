cdceacdc09f00bae6b05115db3b49f14d298ee13
santosjunio96 <santosjunio96@gmail.com>
2025-08-27 18:46:57 +0000

    Super trunfo mestre

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -2,9 +2,7 @@
  
 int main() {
     
-    /*-----------------------------------------
-                        Carta 01
-    -----------------------------------------*/                
+    // Variaveis carta 01           
     char Estado_01 [50];
     char CodigoDaCarta_01 [50];
     char NomeDaCidade_01 [50];
@@ -56,8 +54,6 @@ int main() {
     SuperPoder_01 = Populacao_01 + Area_01 + PIB_01 + NumeroDePontosTuristicos_01 + PibPerCapita_01 + DesindadeMenorPopulacao_01;
 
 
-
-
     printf("----------Carta 01---------\n");
     printf("Estado: %s \n", Estado_01); 
     printf("Codigo da carta: %s \n", CodigoDaCarta_01); 
@@ -70,18 +66,10 @@ int main() {
     printf("Pib per capita: %.2f \n", PibPerCapita_01); // Divisão PIB / população
     printf("Menor densidade populacional: %f \n", DesindadeMenorPopulacao_01);
     printf("Super Poder: %.2f \n", SuperPoder_01);
-
-
     printf("---------------------------------------\n");
-
-
     // coloquei esse printf apenas para deixar o codigo visivelmente mais atraente apos rodado
-
-
-
-
-    /*
-    Codigo com dados sem usar o scanf  
+    
+    /* Codigo com dados sem usar o scanf  
 
     char Estado_01 [50] = "A";
     char CodigoDaCarta_01 [50] = "A01";
@@ -102,17 +90,9 @@ int main() {
     printf("PIB: %.2f \n", PIB_01);
     printf("Pontos turisticos: %d \n", NumeroDePontosTuristicos_01);
     printf("Densidade populacional: %.2f\n",DensidadePopulacional_01);
-    printf("Pib per capita: %.2f\n", PibPerCapita_01);
-
-    */
-
-
+    printf("Pib per capita: %.2f\n", PibPerCapita_01); */
     
-    /*-----------------------------------------
-                        Carta 02:
-    -------------------------------------------*/
-
-    
+    // Variaveis carta 02  
     char Estado_02 [50];
     char CodigoDaCarta_02 [50];
     char NomeDaCidade_02 [50];
@@ -161,9 +141,6 @@ int main() {
     SuperPoder_02 = Populacao_02 + Area_02 + PIB_02 + NumeroDePontosTuristicos_02 + PibPerCapita_02 + DesindadeMenorPopulacao_02;
 
 
-
-
-
     printf("----------Carta 02---------\n");
     printf("Estado: %s \n", Estado_02);
     printf("Codigo da carta: %s \n", CodigoDaCarta_02);
@@ -179,8 +156,7 @@ int main() {
     printf("-----------------------------------------------\n");
     // coloquei esse printf apenas para deixar o codigo visivelmente mais atraente apos rodado
 
-  /* 
-    Codigo com dados sem usar o scanf  
+  /* Codigo com dados sem usar o scanf  
 
     char Estado_02 [50] = "B";
     char CodigoDaCarta_02 [50] = "B02";
@@ -201,13 +177,171 @@ int main() {
     printf("PIB: %.2f \n", PIB_02);
     printf("Pontos turisticos: %d \n", NumeroDePontosTuristicos_02);
     printf("Densidade populacional: %.2f\n",DensidadePopulacional_02); // Divisão população / area
-    printf("Pib per capita: %.2f\n", PibPerCapita_02); // Divisão PIB / população
-*/
+    printf("Pib per capita: %.2f\n", PibPerCapita_02); // Divisão PIB / população*/
+     
+    int Opcao, Atributo1, Atributo2, ResultadoPopulacao,ResultadoTurista;
+    float ResultadoArea,ResultadoPIB,ResultadoDensidade,ResultadoPercapita, ResultadoSuperPoder;
+        
+    printf("Game Super trunfo\n");
+    printf("1. Iniciar Jogo\n");
+    printf("2. Regras Jogo\n");
+    printf("3. Sair Jogo\n");
+    printf("Escolha uma opção: ");
+    scanf("%d",&Opcao);
+    printf("----------------------------------------------------------------------------------\n");
 
-    //-------------------------------------------------
+    switch (Opcao){
+    case 1:
+      printf("Escolher atributos para duelo\n");
+      printf("1. População \n");
+      printf("2. Area \n");
+      printf("3. PIB \n");
+      printf("4. Pontos turisticos \n");
+      printf("5. Densidade populacional \n");
+      printf("5. Pib per capita \n");
+      printf("6. Super Poder \n");
+      printf("----------------------------------------------------------------------------------\n");
+      printf("Escolha o 1° atributo: ");
+      scanf("%d", &Atributo1);
+      printf("Escolha o 2° atributo: ");
+      scanf("%d", &Atributo2);
 
+      printf("-------------------------------Atributo 01---------------------------------------\n");
+      switch (Atributo1){
+        case 1:
+          printf("População carta 1: %d\n",Populacao_01);
+          printf("População carta 2: %d\n",Populacao_02);
+          ResultadoPopulacao = (Populacao_01 > Populacao_02) ? 1: 0;
+          if (ResultadoPopulacao == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}
+          break;
+        case 2:
+          printf("Area carta 1: %.2f\n",Area_01);
+          printf("Area carta 2: %.2f\n",Area_02);
+          ResultadoArea = (Area_01 > Area_02) ? 1: 0;
+          if (ResultadoArea == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                
+          break;
+        case 3:
+          printf("PIB carta 1: %.2f\n",PIB_01);
+          printf("PIB carta 2: %.2f\n",PIB_02);
+          ResultadoPIB = (PIB_01 > PIB_02) ? 1: 0;
+          if (ResultadoPIB == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 4:
+          printf("Pontos turisticos carta 1: %d\n",NumeroDePontosTuristicos_01);
+          printf("Pontos turisticos carta 2: %d\n",NumeroDePontosTuristicos_02);
+          ResultadoTurista = (NumeroDePontosTuristicos_01 > NumeroDePontosTuristicos_02) ? 1: 0;
+          if (ResultadoTurista == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 5:
+          printf("Menor densisade populacional carta 1: %f\n",DesindadeMenorPopulacao_01);
+          printf("Menor densidade populacional carta 2: %f\n",DesindadeMenorPopulacao_02);
+          ResultadoDensidade = (DesindadeMenorPopulacao_01 < DesindadeMenorPopulacao_02) ? 1: 0;
+          if (ResultadoDensidade == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 6:
+          printf("Super Poder carta 1: %.2f\n", SuperPoder_01);
+          printf("Super Poder carta 2: %.2f\n", SuperPoder_02);
+          ResultadoPopulacao = (SuperPoder_01 > SuperPoder_02) ? 1: 0;
+          if (ResultadoSuperPoder == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+      default:
+        break;}
 
-
+      printf("-------------------------------Atributo 02---------------------------------------\n");
+      switch (Atributo2){
+        case 1:
+          printf("População carta 1: %d\n",Populacao_01);
+          printf("População carta 2: %d\n",Populacao_02);
+          ResultadoPopulacao = (Populacao_01 > Populacao_02) ? 1: 0;
+          if (ResultadoPopulacao == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}
+          break;
+        case 2:
+          printf("Area carta 1: %.2f\n",Area_01);
+          printf("Area carta 2: %.2f\n",Area_02);
+          ResultadoArea = (Area_01 > Area_02) ? 1: 0;
+          if (ResultadoArea == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                
+          break;
+        case 3:
+          printf("PIB carta 1: %.2f\n",PIB_01);
+          printf("PIB carta 2: %.2f\n",PIB_02);
+          ResultadoPIB = (PIB_01 > PIB_02) ? 1: 0;
+          if (ResultadoPIB == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 4:
+          printf("Pontos turisticos carta 1: %d\n",NumeroDePontosTuristicos_01);
+          printf("Pontos turisticos carta 2: %d\n",NumeroDePontosTuristicos_02);
+          ResultadoTurista = (NumeroDePontosTuristicos_01 > NumeroDePontosTuristicos_02) ? 1: 0;
+          if (ResultadoTurista == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 5:
+          printf("Menor densisade populacional carta 1: %f\n",DesindadeMenorPopulacao_01);
+          printf("Menor densidade populacional carta 2: %f\n",DesindadeMenorPopulacao_02);
+          ResultadoDensidade = (DesindadeMenorPopulacao_01 < DesindadeMenorPopulacao_02) ? 1: 0;
+          if (ResultadoDensidade == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+        case 6:
+          printf("Super Poder carta 1: %.2f\n", SuperPoder_01);
+          printf("Super Poder carta 2: %.2f\n", SuperPoder_02);
+          ResultadoPopulacao = (SuperPoder_01 > SuperPoder_02) ? 1: 0;
+          if (ResultadoSuperPoder == 1){
+            printf("Carta 1 Ganhou! \n");}
+          else{
+            printf("Carta 2 Ganhou! \n");}                    
+          break;
+      default:
+          break;}
+      break;
+    case 2:
+      printf("1° Escolha de Dois Atributos numéricos diferentes para a comparação das cartas,\n");
+      printf(" através de menus interativos.\n");
+      printf("----------------------------------------------------------------------------------\n");
+      printf("2° Para cada atributo, a regra geral é: vence a carta com o maior valor.\n");
+      printf("Com exceção da Densidade Demográfica, onde vence a carta com o menor valor.\n");
+      printf("----------------------------------------------------------------------------------\n");
+      printf("3° Após comparar os dois atributos individualmente, o sistema ira somar os \n");
+      printf("valores dos atributos para cada carta. A carta com a maior soma vence a rodada.\n");
+      printf("----------------------------------------------------------------------------------\n");
+      break;
+    case 3:
+      printf("Jogo ençerrado \n");
+    break;
+      default:
+        break;}
+      
+/*
     printf("-------------Carta Vencedora-------------------\n");
     printf("-----------------------------------------------\n");
 
@@ -269,7 +403,7 @@ int main() {
 
     return 0;
 
-
+*/
 
 
 }
